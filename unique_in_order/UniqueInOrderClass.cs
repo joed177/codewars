ee47dd452d23f539036d23704a7325bcac6bd9ea
joed177 <48810340+joed177@users.noreply.github.com>
2019-06-14 13:54:21 -0400

    Add files via upload

---
+++ unique_in_order/UniqueInOrderClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace unique_in_order
+{
+    public class UniqueInOrderClass
+    {
+        private string theString;
+
+    
+        public UniqueInOrderClass()
+        {
+            
+        }
+        public static char[] UniqueInOrder(string s)
+        {
+            List<string> outString = new List<string>();
+            char[] sArray = s.ToCharArray();
+            int k = sArray.Length;
+
+            // outArray set to length of starting string because there could be no duplicates
+            char[] outArray = new char[k]; 
+            int j = 0;
+            outArray[j] = sArray[0];
+            for(int i = 1;i<sArray.Length;i++)
+            {
+                if(outArray[j] == sArray[i])
+                {
+                    i++;
+                }
+                else
+                {
+                    outArray[++j] = sArray[i];
+                }
+  
+            }
+            return outArray;
+         
+        }
+
+    }
+}
